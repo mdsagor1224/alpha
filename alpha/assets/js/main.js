ec726c3ca7a200cdf721484ff1ffa907e4f9c728
Shagor Ahmed <mdsagorfreelancer@gmail.com>
2024-03-14 11:27:13 +0600

    alpha1

---
+++ alpha/assets/js/main.js
@@ -0,0 +1,13 @@
+; (function($){
+    $(document).ready(function(){
+        $(".popup").each(function(){
+
+            var image = $(this).find("img").attr("src");
+            $(this).attr("href",image);
+
+
+
+        });
+    });
+
+})(jQuery);
